2d93e1b79ce23921b5b349679768bc8c5aac957b
harryhasai <harryhasai@qq.com>
2018-11-19 12:07:31 +0800

    first commit

--- app/src/main/java/com/pywl/ebangbang_rider/utils/NotificationUtil.java
+++ app/src/main/java/com/pywl/ebangbang_rider/utils/NotificationUtil.java
@@ -1,15 +1,13 @@
 package com.pywl.ebangbang_rider.utils;
 
-import android.app.Notification;
 import android.app.NotificationChannel;
 import android.app.NotificationManager;
 import android.content.Context;
+import android.graphics.BitmapFactory;
 import android.os.Build;
-import android.speech.tts.TextToSpeech;
+import android.support.v4.app.NotificationCompat;
 
 import com.pywl.ebangbang_rider.R;
-
-import java.util.Locale;
 
 import static android.content.Context.NOTIFICATION_SERVICE;
 
@@ -24,19 +22,21 @@ public class NotificationUtil {
      */
     public static void createNotification(Context context) {
         NotificationManager notificationManager = (NotificationManager) context.getSystemService(NOTIFICATION_SERVICE);
-        Notification.Builder builder;
+        NotificationCompat.Builder builder;
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
             String channelId = "ride_notify";
             String channelName = "接单通知";
             NotificationChannel channel = new NotificationChannel(channelId, channelName, NotificationManager.IMPORTANCE_HIGH);
             notificationManager.createNotificationChannel(channel);
-            builder = new Notification.Builder(context, channelId);
+            builder = new NotificationCompat.Builder(context, channelId);
+            builder.setChannelId(channelId);
         } else {
-            builder = new Notification.Builder(context);
+            builder = new NotificationCompat.Builder(context);
         }
 
         builder.setTicker("new message")
                 .setSmallIcon(R.drawable.ic_logo)
+                .setLargeIcon(BitmapFactory.decodeResource(context.getResources(), R.drawable.ic_logo))
                 .setAutoCancel(true)
                 .setWhen(System.currentTimeMillis())
                 .setContentTitle("收到一条接单消息")
